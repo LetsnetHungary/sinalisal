cd7927120f97a5307f281306627fec277116a0ab
bujtorbalint <bujtorbalint@letsnet.hu>
2017-03-30 23:36:17 +0200

    sidebar vázlat

--- includes/views/sidebar.php
+++ includes/views/sidebar.php
@@ -1,1 +1,12 @@
-itt egy sidebar
+<?php require ("/includes/css/sidebar.css"); ?>
+
+<div class="big-container">
+  <div class="list-container">
+    <ul>
+      <li><a href="#">List item no1</a></li>
+      <li><a href="#">list item no2</a></li>
+      <li><a href="#">list item no3</a></li>
+      <li><a href="#">list item no4</a></li>
+    </ul>
+  </div>
+</div>
