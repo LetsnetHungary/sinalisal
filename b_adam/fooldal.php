8d7aee240ae717257ddf5371c432b7c0ddf0e7d4
ADAMS-PC\balas <balassaadi@gmail.com>
2017-03-30 17:56:11 +0200

    fooldal vazlat
    -"-

---
+++ b_adam/fooldal.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
+    <link rel="stylesheet" type ="text/css" href ="css/fooldal.css">
+    <title>Főoldal</title>
+  </head>
+  <body>
+    <header>
+      HEADER
+    </header>
+    <main>
+      <div class="front-pic">
+        <div class="left-side">
+          <div class="layer-container">
+            <div class="layer">
+                <span>hatrya szövege</span>
+            </div>
+          </div>
+        </div>
+        <div class="right-side">
+        </div>
+      </div>
+      <div class="big-container">
+        <div class="container">
+          <div class="title">
+              <h2>Nyitvatartás</h2>
+          </div>
+          <div class="content">
+            <span>
+                Fogászati implantátum szaküzletünk Budapest II.
+                kerületében (Rózsadomb) a Berkenye utca 8/b szám
+                alatt található. Hétköznapokon 9-16 óráig (pénteken
+                15 óráig). Amennyiben ettől eltérő időpontban szeretne
+                fogászati implantátumot, felépítményt vagy csontpótló
+                anyagot vásárolni, úgy azt előzetes telefonon
+                történő egyeztetést követően szinte bármikor
+                megteheti.
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="big-container gray-container">
+        <div class="container">
+          <div class="title">
+              <h2>Megközelítés gyalogosan</h2>
+          </div>
+          <div class="content">
+            <span>
+                Nyugati pályaudvartól induló (Margit-híd pesti és budai hídfőjénél is megálló) 91, 191, 291-es buszjáratok bármelyikével a Vérhalom (Mandula) utcánál leszállva 3 perces sétával érheti el üzletünket, vagy a Batthyány térről induló 11, 111 járatokról az Eszter utcánál leszállva szintén 3 perc alatt juthat el üzletünkhöz.
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="big-container">
+        <div class="container">
+          <div class="title">
+              <h2 style="text-align: center">Hogyan döntsem el melyik implantátumrendszert válasszam?</h2>
+          </div>
+          <div class="content">
+            <span>
+              Magyarországon minden évben újabb és újabb implantátumcsaládok jelennek meg a piacon, ami nagyon örvendetes a vásárlók szempontjából, hiszen egyre nagyobb a választék. Ugyanakkor egyre nehezebb is a döntés: vajon melyik rendszert válasszam? Hiszen mindenki tudja, hogy különbség van implantátum és implantátum között: tulajdonságaiban, felépítményeiben és persze az árban.
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="big-container gray-container">
+        <div class="container">
+          <div class="title">
+              <h2>De tényleg az ár a legfontosabb?</h2>
+          </div>
+          <div class="content">
+            <span>
+              Sokan elsősorban ezt tartják elsődlegesen szem előtt, és kevésbé foglalkoznak azzal, hogy mit is vesznek valójában..
+              Mi úgy gondoljuk, hogy a megbízható minőség, precizitás, bizonyítottan egészségre ártalmatlan anyag összetétel, pontos és kedves kiszolgálás, valamint a több, mint 20 éves piaci jelenlét az, amely garantálja, hogy az általunk forgalmazott implantátumokkal nemcsak a fogorvosok, de a páciensek is elégedettek lesznek hosszútávon is.
+            </span>
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer>
+
+    </footer>
+  </body>
+</html>
