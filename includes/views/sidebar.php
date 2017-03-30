7d068c5f0fe533f28c787c18b63be24ebd5c208f
bujtorbalint <bujtorbalint@letsnet.hu>
2017-03-31 00:33:38 +0200

    további változtatások a sidebarban

--- includes/views/sidebar.php
+++ includes/views/sidebar.php
@@ -1,12 +1,25 @@
-<?php require ("/includes/css/sidebar.css"); ?>
-
 <div class="big-container">
-  <div class="list-container">
-    <ul>
-      <li><a href="#">List item no1</a></li>
-      <li><a href="#">list item no2</a></li>
-      <li><a href="#">list item no3</a></li>
-      <li><a href="#">list item no4</a></li>
-    </ul>
-  </div>
+
+    <h1>A listás elemek</h1>
+
+      <div class="list-container">
+        <ul>
+          <li><a href="#">List item no1</a></li>
+          <li><a href="#">list item no2</a></li>
+          <li><a href="#">list item no3</a></li>
+          <li><a href="#">list item no4</a></li>
+        </ul>
+      </div>
+
+    <h1>B listás elemek</h1>
+
+      <div class="list-container">
+        <ul>
+          <li><a href="#">List item no1</a></li>
+          <li><a href="#">list item no2</a></li>
+          <li><a href="#">list item no3</a></li>
+          <li><a href="#">list item no4</a></li>
+        </ul>
+      </div>
+
 </div>
