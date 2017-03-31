6ff524e30d086169caaa7cc6af2c94794b553807
Szabó Gábor <szabo.gabor@letsnet.hu>
2017-03-31 15:50:44 +0200

    header test

--- includes/views/header.php
+++ includes/views/header.php
@@ -1,3 +1,58 @@
-<header>
-  <h1 style="font-size: 70px">HEADER</h1>
-</header>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="includes/css/header.css">
+    <title></title>
+
+  </head>
+  <body>
+
+    <div class = "Main">
+
+      <div class="Space">
+
+        <a href="">
+          <div class="Logo">
+          Logoka
+          </div>
+        </a>
+
+      </div>
+
+      <div class="Header">
+
+        <a href="">
+          <div class="Menu">
+            Menu1
+          </div>
+        </a>
+
+        <a href="">
+          <div class="Menu">
+            Menu2
+          </div>
+        </a>
+
+        <a href="">
+          <div class="Menu">
+            Menu3
+          </div>
+        </a>
+
+        <a href="">
+          <div class="Menu">
+            Menu4
+          </div>
+        </a>
+
+    </div>
+
+    </div>
+    <script>
+
+    </script>
+
+
+  </body>
+</html>
