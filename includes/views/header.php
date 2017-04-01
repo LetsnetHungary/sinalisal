a2736e026bcf665a507497a775b79cc2c332d05f
hegelakos <hegel.akos@letsnet.hu>
2017-04-01 17:04:27 +0200

    bootstrap slider

--- includes/views/header.php
+++ includes/views/header.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="includes/css/header.css">
-    <title></title>
 
-  </head>
-  <body>
 
     <div class = "Main">
 <!--
--- index.php
+++ index.php
@@ -3,8 +3,22 @@
 <html>
   <head>
     <meta charset="utf-8">
+
     <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
-    <link rel="stylesheet" type ="text/css" href ="includes/css/fooldal.css">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+
+  <link rel="stylesheet" type ="text/css" href ="includes/css/fooldal.css">
+  <link rel="stylesheet" type ="text/css" href ="includes/css/header.css">
+
+
+
+
+
     <title>Főoldal</title>
   </head>
   <body>
@@ -21,15 +35,50 @@
         <div class="left-side">
           <div class="layer-container">
             <div class="layer">
-                <div class="layer-text-container">
-                  <div class="layer-text">
-                    <div class="dot-holder">
-                    </div>
-                    <div class="text-holder">
-                        Prémium precíziós implantátum rendszer az Egyesült Államok piacvezető gyártójától
-                    </div>
-                  </div>
+            <style>
+
+              .layer {
+                height: 250px;
+              }
+
+              .carousel {
+                height: 100%;
+                width: 100%;
+              }
+
+              .sliderimg {
+                width: 500px;
+                height: 250px;
+                object-fit: cover;
+              }
+
+              .objfit {
+                margin: 0;
+                padding: 0;
+              }
+
+            </style>
+              <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active" >
+                  <img class="d-block img-fluid sliderimg" src="http://placehold.it/500x250" alt="First slide" style = "opacity : 0;">
                 </div>
+                <div class="carousel-item ">
+                  <img class="d-block img-fluid sliderimg" src="http://placehold.it/400x200" alt="Second slide" style = "opacity : 0;">
+                </div>
+                <div class="carousel-item ">
+                  <img class="d-block img-fluid sliderimg" src="http://placehold.it/400x200" alt="Third slide" style = "opacity : 0;">
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+              </div>
             </div>
           </div>
         </div>
@@ -40,7 +89,6 @@
 
         <div class="img-container">
           <div class="img">
-            <img src="includes/img/clock.jpg" alt="" class="clock image">
           </div>
         </div>
 
