f267f60b6c6d9d0db8f6b0d9e56a63dd8048b8e3
ADAMS-PC\balas <balassaadi@gmail.com>
2017-04-07 18:07:34 +0200

    video_slider_hopefully

--- catalog.php
+++ catalog.php
@@ -7,6 +7,12 @@
     <link rel="stylesheet" href="includes/css/sidebar.css">
       <link rel="stylesheet" href="includes/css/footer.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
     <title></title>
   </head>
   <body>
@@ -27,7 +33,7 @@
           <div class="ebook-flex">
 
             <div class="catalog">
-              <iframe class="buk" src="https://e.issuu.com/issuu-reader3-embed-files/stable/embed.html#iargs=eyJkb2N1bWVudElkIjoiMTQwMzE0MTI1NTQ4LTQ2NjhlYjY2ODQwMjg1OTgyZDUxZTQ3MzlmNzc0YTA4IiwidXNlcm5hbWUiOiJpbXBsYW50MDEiLCJkb2N1bWVudE5hbWUiOiJlbl9pbnRlcmFjdGl2ZV9jYXRhbG9nIiwiaW5pdGlhbFBhZ2VOdW1iZXIiOiIxIiwiZW1iZWRJZCI6IjEyOTExNjUvNzA5NTgwNyIsImVtYmVkQ29uZmlnIjp7Imhvc3RVcmwiOiJodHRwOi8vaW1wbGFudGRpcmVjdC5odS9pbnRlcmFjdGl2ZV9pbXBsYW50YXR1bV9rYXRhbG9ndXMuaHRtIiwiaG9zdFJlZmVycmVyIjoiaHR0cDovL2ltcGxhbnRkaXJlY3QuaHUva2F0YWxvZ3Vzb2suaHRtIiwiZW1iZWRJZCI6IjEyOTExNjUvNzA5NTgwNyIsImVtYmVkVHlwZSI6InNjcmlwdCIsInZpZXdNb2RlIjoiRE9VQkxFX1BBR0VfU1BSRUFEIiwiYXV0b0ZsaXAiOmZhbHNlLCJiYWNrZ3JvdW5kQ29sb3IiOiIjZmZmZmZmIiwibG9nbyI6IiIsImJhY2tncm91bmRJbWFnZSI6IiIsImJhY2tncm91bmRTdHJldGNoIjp0cnVlLCJ0aXRsZUJhckVuYWJsZWQiOmZhbHNlLCJzaGFyZU1lbnVFbmFibGVkIjp0cnVlLCJzaGFyZUJ1dHRvbkVuYWJsZWQiOnRydWUsInByb1NpZGViYXJFbmFibGVkIjpmYWxzZX0sImxvYWRTdGFydFRpbWVzdGFtcCI6MTQ5MDk2NDE2NjU3MywiZmVhdHVyZXMiOnsic2hvd0lzc3V1QnJhbmRpbmciOnRydWUsImZvcmNlUGFnZUZsaXAiOmZhbHNlfSwiaWRlbnRpZmllciI6IjEyOTExNjUvNzA5NTgwNy0wIn0=">
+              <iframe class="buk" frameborder="0" src="https://e.issuu.com/issuu-reader3-embed-files/stable/embed.html#iargs=eyJkb2N1bWVudElkIjoiMTQwMzE0MTI1NTQ4LTQ2NjhlYjY2ODQwMjg1OTgyZDUxZTQ3MzlmNzc0YTA4IiwidXNlcm5hbWUiOiJpbXBsYW50MDEiLCJkb2N1bWVudE5hbWUiOiJlbl9pbnRlcmFjdGl2ZV9jYXRhbG9nIiwiaW5pdGlhbFBhZ2VOdW1iZXIiOiIxIiwiZW1iZWRJZCI6IjEyOTExNjUvNzA5NTgwNyIsImVtYmVkQ29uZmlnIjp7Imhvc3RVcmwiOiJodHRwOi8vaW1wbGFudGRpcmVjdC5odS9pbnRlcmFjdGl2ZV9pbXBsYW50YXR1bV9rYXRhbG9ndXMuaHRtIiwiaG9zdFJlZmVycmVyIjoiaHR0cDovL2ltcGxhbnRkaXJlY3QuaHUva2F0YWxvZ3Vzb2suaHRtIiwiZW1iZWRJZCI6IjEyOTExNjUvNzA5NTgwNyIsImVtYmVkVHlwZSI6InNjcmlwdCIsInZpZXdNb2RlIjoiRE9VQkxFX1BBR0VfU1BSRUFEIiwiYXV0b0ZsaXAiOmZhbHNlLCJiYWNrZ3JvdW5kQ29sb3IiOiIjZmZmZmZmIiwibG9nbyI6IiIsImJhY2tncm91bmRJbWFnZSI6IiIsImJhY2tncm91bmRTdHJldGNoIjp0cnVlLCJ0aXRsZUJhckVuYWJsZWQiOmZhbHNlLCJzaGFyZU1lbnVFbmFibGVkIjp0cnVlLCJzaGFyZUJ1dHRvbkVuYWJsZWQiOnRydWUsInByb1NpZGViYXJFbmFibGVkIjpmYWxzZX0sImxvYWRTdGFydFRpbWVzdGFtcCI6MTQ5MDk2NDE2NjU3MywiZmVhdHVyZXMiOnsic2hvd0lzc3V1QnJhbmRpbmciOnRydWUsImZvcmNlUGFnZUZsaXAiOmZhbHNlfSwiaWRlbnRpZmllciI6IjEyOTExNjUvNzA5NTgwNy0wIn0=">
             </iframe>
 
             </div>
@@ -38,13 +44,81 @@
           </div>
 
           <div class="yt-flex">
-            <div class="yt-item"><iframe class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
-            <div class="yt-item"><iframe class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
-            <div class="yt-item"><iframe class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
-            <div class="yt-item"><iframe class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
-            <div class="yt-item"><iframe class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
+            <div class="cont">
+              <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#myCarousel" data-slide-to="1"></li>
+                  <li data-target="#myCarousel" data-slide-to="2"></li>
+                  <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
 
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
 
+                  <div class="item active">
+                    <img src="http://placehold.it/550x500" alt="Chania" width="460" height="345">
+                    <div class="carousel-caption">
+                      <div class="carousel-text">
+                        <div class="yt-item"><iframe frameborder="0" class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
+
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="item">
+                    <img src="http://placehold.it/550x500" alt="Chania" width="460" height="345">
+                    <div class="carousel-caption">
+                      <div class="carousel-text">
+                       <div class="yt-item"><iframe frameborder="0" class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
+
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="item">
+                    <img src="http://placehold.it/550x500" alt="Flower" width="460" height="345">
+                    <div class="carousel-caption">
+                      <div class="carousel-text">
+                        <div class="yt-item"><iframe frameborder="0" class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
+
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="item">
+                    <img src="http://placehold.it/550x500" alt="Flower" width="460" height="345">
+                    <div class="carousel-caption">
+                      <div class="carousel-text">
+                        <div class="yt-item"><iframe frameborder="0" class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
+
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="item">
+                    <img src="http://placehold.it/550x500" alt="Flower" width="460" height="345">
+                    <div class="carousel-caption">
+                      <div class="carousel-text">
+                        <div class="yt-item"><iframe frameborder="0" class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
+
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+
+                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
+            </div>
           </div>
 
 
