ec499255a5b5cde58f66ceb27451d34af69af601
ADAMS-PC\balas <balassaadi@gmail.com>
2017-04-27 15:46:09 +0200

    masterpiece
    masterpiece indeed

--- catalog.php
+++ catalog.php
@@ -8,21 +8,19 @@
       <link rel="stylesheet" href="includes/css/footer.css">
       <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
--->
     <title></title>
   </head>
   <body>
 
 
+
 <!--
-    <?php require("includes/views/header.php");?>
     <main>
       <div class="flex-container">
         <div class="sidebar-place">
-          <?php require("includes/views/sidebar.php");?>
         </div>
 
         <div class="main-flex">
@@ -33,16 +31,7 @@
             <hr class="hr1">
           </div>
 
-          <div class="text_holder">
-            <div class="termek_text">
-              <p>
-              A kompatibilitási termékvonal a saját fejlesztésű implantátum választékot egészíti ki úgy, hogy a drága gyártók (Zimmer Dental, MIS, Alpha Bio, AB Dental, BioHorizons) termékeihez kínál kedvezőbb árú alternatívát, azok megoldásainak továbbfejlesztésével. Az összes kompatibilis implantátum a saját fejlesztésű külső dizájnt kapta. Minden esetben a csavar teljes hossza felületkezelt. Az implantátumok és protetikai elemek kapcsolata az iparban egyedülállóan pontos Precision Fit összeköttetés.
-              </p>
-            </div>
-            <div class="termek_pic">
-              <img src="includes/img/legacy-2.png" alt="">
-            </div>
-          </div>
+
           <div class="catalog-title">
             <p  class="sidebar-p catalog-title-p">Katalógus</p>
             <hr class="hr1">
@@ -61,81 +50,7 @@
           </div>
 
           <div class="yt-flex">
-            <div class="cont">
-              <div id="myCarousel" class="carousel slide" data-ride="carousel">
 
-                <ol class="carousel-indicators">
-                  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                  <li data-target="#myCarousel" data-slide-to="1"></li>
-                  <li data-target="#myCarousel" data-slide-to="2"></li>
-                  <li data-target="#myCarousel" data-slide-to="3"></li>
-                </ol>
-
-
-                <div class="carousel-inner" role="listbox">
-
-                  <div class="item active">
-                    <img src="http://placehold.it/550x500" alt="Chania" width="460" height="345">
-                    <div class="carousel-caption">
-                      <div class="carousel-text">
-                        <div class="yt-item"><iframe frameborder="0" class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
-
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="item">
-                    <img src="http://placehold.it/550x500" alt="Chania" width="460" height="345">
-                    <div class="carousel-caption">
-                      <div class="carousel-text">
-                       <div class="yt-item"><iframe frameborder="0" class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
-
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="item">
-                    <img src="http://placehold.it/550x500" alt="Flower" width="460" height="345">
-                    <div class="carousel-caption">
-                      <div class="carousel-text">
-                        <div class="yt-item"><iframe frameborder="0" class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
-
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="item">
-                    <img src="http://placehold.it/550x500" alt="Flower" width="460" height="345">
-                    <div class="carousel-caption">
-                      <div class="carousel-text">
-                        <div class="yt-item"><iframe frameborder="0" class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
-
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="item">
-                    <img src="http://placehold.it/550x500" alt="Flower" width="460" height="345">
-                    <div class="carousel-caption">
-                      <div class="carousel-text">
-                        <div class="yt-item"><iframe frameborder="0" class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
-
-                      </div>
-                    </div>
-                  </div>
-
-                </div>
-
-                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
-                </a>
-              </div>
-            </div>
           </div>
 
 
@@ -146,23 +61,127 @@
 
   -->
 
-
+<header>
+  <?php require("includes/views/header.php");?>
+</header>
   <main>
-    <div class="container">
+    <div class="container_fuck_bootstrap">
       <div class="sidebar">
-        <div class="sidebar__item">
-          <p class = "sidebar__header">Legacy</p>
-          <p class="sidebar__content">Legacy 1</p>
-          <p class="sidebar__content">Legacy 1</p>
-          <p class="sidebar__content">Legacy 1</p>
-        </div>
 
+          <?php require("includes/views/sidebar.php");?>
       </div>
       <div class="content">
+        <div class="termek_container item-container">
+          <div class="catalog-title">
+            <p  class="title">Legacy 2</p>
+            <hr class="hr1">
+          </div>
+          <div class="text_holder">
+            <div class="termek_text">
+              <p>
+              A kompatibilitási termékvonal a saját fejlesztésű implantátum választékot egészíti ki úgy, hogy a drága gyártók (Zimmer Dental, MIS, Alpha Bio, AB Dental, BioHorizons) termékeihez kínál kedvezőbb árú alternatívát, azok megoldásainak továbbfejlesztésével. Az összes kompatibilis implantátum a saját fejlesztésű külső dizájnt kapta. Minden esetben a csavar teljes hossza felületkezelt. Az implantátumok és protetikai elemek kapcsolata az iparban egyedülállóan pontos Precision Fit összeköttetés.
+              </p>
+            </div>
+            <div class="termek_pic">
+              <img src="includes/img/legacy-2.png" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="catalog_container item-container">
+          <div class="catalog-title">
+            <p  class="title">IzéBizé</p>
+            <hr class="hr1">
+          </div>
 
+            <iframe class="buk" frameborder="0" src="https://e.issuu.com/issuu-reader3-embed-files/stable/embed.html#iargs=eyJkb2N1bWVudElkIjoiMTQwMzE0MTI1NTQ4LTQ2NjhlYjY2ODQwMjg1OTgyZDUxZTQ3MzlmNzc0YTA4IiwidXNlcm5hbWUiOiJpbXBsYW50MDEiLCJkb2N1bWVudE5hbWUiOiJlbl9pbnRlcmFjdGl2ZV9jYXRhbG9nIiwiaW5pdGlhbFBhZ2VOdW1iZXIiOiIxIiwiZW1iZWRJZCI6IjEyOTExNjUvNzA5NTgwNyIsImVtYmVkQ29uZmlnIjp7Imhvc3RVcmwiOiJodHRwOi8vaW1wbGFudGRpcmVjdC5odS9pbnRlcmFjdGl2ZV9pbXBsYW50YXR1bV9rYXRhbG9ndXMuaHRtIiwiaG9zdFJlZmVycmVyIjoiaHR0cDovL2ltcGxhbnRkaXJlY3QuaHUva2F0YWxvZ3Vzb2suaHRtIiwiZW1iZWRJZCI6IjEyOTExNjUvNzA5NTgwNyIsImVtYmVkVHlwZSI6InNjcmlwdCIsInZpZXdNb2RlIjoiRE9VQkxFX1BBR0VfU1BSRUFEIiwiYXV0b0ZsaXAiOmZhbHNlLCJiYWNrZ3JvdW5kQ29sb3IiOiIjZmZmZmZmIiwibG9nbyI6IiIsImJhY2tncm91bmRJbWFnZSI6IiIsImJhY2tncm91bmRTdHJldGNoIjp0cnVlLCJ0aXRsZUJhckVuYWJsZWQiOmZhbHNlLCJzaGFyZU1lbnVFbmFibGVkIjp0cnVlLCJzaGFyZUJ1dHRvbkVuYWJsZWQiOnRydWUsInByb1NpZGViYXJFbmFibGVkIjpmYWxzZX0sImxvYWRTdGFydFRpbWVzdGFtcCI6MTQ5MDk2NDE2NjU3MywiZmVhdHVyZXMiOnsic2hvd0lzc3V1QnJhbmRpbmciOnRydWUsImZvcmNlUGFnZUZsaXAiOmZhbHNlfSwiaWRlbnRpZmllciI6IjEyOTExNjUvNzA5NTgwNy0wIn0=">
+          </iframe>
+
+        </div>
+        <div class="youtube_container item-container">
+          <div class="catalog-title">
+            <p  class="title">Videók</p>
+            <hr class="hr1">
+          </div>
+          <div class="cont">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+              <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+                <li data-target="#myCarousel" data-slide-to="3"></li>
+              </ol>
+
+
+              <div class="carousel-inner" role="listbox">
+
+                <div class="item active">
+                  <img src="http://placehold.it/550x500" alt="Chania" width="460" height="345">
+                  <div class="carousel-caption">
+                    <div class="carousel-text">
+                      <div class="yt-item"><iframe frameborder="0" class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
+
+                    </div>
+                  </div>
+                </div>
+
+                <div class="item">
+                  <img src="http://placehold.it/550x500" alt="Chania" width="460" height="345">
+                  <div class="carousel-caption">
+                    <div class="carousel-text">
+                     <div class="yt-item"><iframe frameborder="0" class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
+
+                    </div>
+                  </div>
+                </div>
+
+                <div class="item">
+                  <img src="http://placehold.it/550x500" alt="Flower" width="460" height="345">
+                  <div class="carousel-caption">
+                    <div class="carousel-text">
+                      <div class="yt-item"><iframe frameborder="0" class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
+
+                    </div>
+                  </div>
+                </div>
+
+                <div class="item">
+                  <img src="http://placehold.it/550x500" alt="Flower" width="460" height="345">
+                  <div class="carousel-caption">
+                    <div class="carousel-text">
+                      <div class="yt-item"><iframe frameborder="0" class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
+
+                    </div>
+                  </div>
+                </div>
+
+                <div class="item">
+                  <img src="http://placehold.it/550x500" alt="Flower" width="460" height="345">
+                  <div class="carousel-caption">
+                    <div class="carousel-text">
+                      <div class="yt-item"><iframe frameborder="0" class="yutub" src="//www.youtube.com/embed/lM8aFiqjN-Y" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"></iframe></div>
+
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+              <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </main>
-    <?php require("includes/views/footer.php");?>
+  <footer>
+    <?php require("includes/views/footer.php");?></footer>
   </body>
 </html>
