bb8d09da5b6a0e4adbee30c673a46c38ce6cfed5
hegelakos <hegel.akos@letsnet.hu>
2017-05-03 14:17:50 +0200

    Csontpotlok meg minden

--- App/_controllers/ViewControllers/Csontpotlok.php
+++ App/_controllers/ViewControllers/Csontpotlok.php
@@ -2,6 +2,15 @@
 	class Csontpotlok extends CoreApp\ViewController {
 		public function __construct() {
 			parent::__construct(__CLASS__);
+			$this->v->sidebar = CoreApp\CMS::getCMSContent("sidebar_csp");
+
+			$_GET["main"] = true;
+		}
+
+
+		public function rendszerek($r) {
+			$this->v->rendszer = CoreApp\CMS::getCMSContent($r);
+			$_GET["main"] = false;
 		}
 
 		/*
--- _views/Csontpotlok/index.php
+++ _views/Csontpotlok/index.php
@@ -1,1 +1,27 @@
-INDEX BODY
+<?php $sidebar = json_decode($this->sidebar); ?>
+<header>
+  <?php require("_views/_includes/_headers/header.php");?>
+</header>
+<main>
+  <div class="container_fuck_bootstrap">
+    <div class="sidebar">
+        <?php require("_views/_includes/_headers/sidebar.php");?>
+    </div>
+    <div class="content">
+      <?php
+
+        if($main) {
+          require("_views/_includes/csontpotlok_main.php");
+        }
+        else {
+          require("_views/_includes/csontpotlok.php");
+        }
+
+      ?>
+    </div>
+  </div>
+</main>
+<footer>
+  <?php require("_views/_includes/_footers/footer.php");?></footer>
+</body>
+</html>
