2ecd05a49f6bb47cb8100e1b7f19a3eed767ef9b
hegelakos <hegel.akos@letsnet.hu>
2017-05-03 07:06:05 +0200

    implantatumok + docx

--- App/_controllers/ViewControllers/Implantatumok.php
+++ App/_controllers/ViewControllers/Implantatumok.php
@@ -3,11 +3,13 @@
 		public function __construct() {
 			parent::__construct(__CLASS__);
 			$this->v->sidebar = CoreApp\CMS::getCMSContent("sidebar");
-			$this->v->rendszer = CoreApp\CMS::getCMSContent("Legacy1");
+			$this->v->rendszer = CoreApp\CMS::getCMSContent("Implantatumok");
+			$_GET["main"] = true;
 		}
 
 		public function rendszerek($r) {
 			$this->v->rendszer = CoreApp\CMS::getCMSContent($r);
+			$_GET["main"] = false;
 		}
 
 		/*
