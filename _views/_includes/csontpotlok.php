477aac729fa1f545388a3903f5d5e34230ea48ef
ADAMS-PC\balas <balassaadi@gmail.com>
2017-05-05 22:39:23 +0200

    fair enough

--- _views/_includes/csontpotlok.php
+++ _views/_includes/csontpotlok.php
@@ -7,43 +7,56 @@
       <div class="legacy__intro">
         <?php echo($rendszer->system) ?>
       </div>
-      <div class="legacy__intro">
-        Méretek:
-      </div>
-  <div class="text_holder">
-    <div class="termek_text">
-      <?php foreach ($meretek as $key => $value): ?>
-        <div class="szolgaltatasok">
-            <div class="dot-holder orange">
-            </div>
-            <div class="szolg">
-              <?php echo $value; ?>
-            </div>
+    <div class="content__container">
+      <div class="text__container">
+        <div class="legacy__intro">
+          Méretek:
         </div>
-      <?php endforeach; ?>
-    </div>
-</div>
-      <div class="legacy__intro">
-        Előnyök:
-      </div>
-  <div class="text_holder">
-    <div class="termek_text">
-      <?php foreach ($szolg as $key => $value): ?>
-        <div class="szolgaltatasok">
-            <div class="dot-holder orange">
-            </div>
-            <div class="szolg">
-              <?php echo $value; ?>
-            </div>
+        <div class="text_holder">
+          <div class="termek_text">
+            <?php foreach ($meretek as $key => $value): ?>
+              <div class="szolgaltatasok">
+                  <div class="dot-holder orange">
+                  </div>
+                  <div class="szolg">
+                    <?php echo $value; ?>
+                  </div>
+              </div>
+            <?php endforeach; ?>
+          </div>
         </div>
-      <?php endforeach; ?>
-    </div>
-    <div class="termek_pic">
-      <img src="<? echo $rendszer->image; ?>" alt="">
+        <div class="legacy__intro">
+          Előnyök:
+        </div>
+        <div class="text_holder">
+          <div class="termek_text">
+            <?php foreach ($szolg as $key => $value): ?>
+              <div class="szolgaltatasok">
+                  <div class="dot-holder orange">
+                  </div>
+                  <div class="szolg">
+                    <?php echo $value; ?>
+                  </div>
+              </div>
+            <?php endforeach; ?>
+          </div>
+        </div>
+      </div>
+      <div class="termek_pic">
+        <img src="<? echo $rendszer->image; ?>" alt="">
+      </div>
     </div>
   </div>
-</div>
-<p class = "catalog_mini"><a target = "_blank" href="<?php echo $rendszer->catalog; ?>">Letölthető katalógus</a></p>
+  <a target = "_blank" href="<?php echo $rendszer->catalog; ?>">
+  <div class="download_link">
+    <div class="icon">
+      <img src="../../_assets/img/download-icon2.png" alt="">
+    </div>
+    <p class = "catalog_mini">
+      Letölthető katalógus
+    </p>
+  </div>
+</a>
 <div class="" style = "margin-bottom: 15%;">
 
 </div>
